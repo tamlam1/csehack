3b4b3070a81f0fa1de4ace9a3d3cff12e195cfac
Tam Lam <tamhailam1@gmail.com>
2021-07-17 16:08:58 +1000

    made button collect text from textfield

--- frontend/sandbox/src/App.js
+++ frontend/sandbox/src/App.js
@@ -1,14 +1,41 @@
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Box } from '@material-ui/core';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+  const [transcript, setTranscript] = React.useState(''); 
   return (
-    <div className="App">
+    <Box display="flex" flexDirection="column" className="App" alignItems="center" justifyContent="center">
       <h1>SAND BOX</h1>
-      <Button variant="contained" color="primary">
-        Upload
-      </Button>
-    </div>
+      <Box display="flex" flexDirection="column" className="App" alignItems="center" justifyContent="center">
+        <TextField
+          name="transcript"
+          placeholder="Enter lecture transcript here..."
+          variant="outlined"
+          multiline
+          rows={10}
+          rowsMax={10}
+          style={{margin:"5px", width:"800px"}}
+          onChange={(e) => {
+            setTranscript(e.target.value);
+           
+          
+          }}
+          // className={classes.textarea}
+          value={transcript}
+        />
+        <Button 
+          variant="contained"
+          color="primary"
+          style={{margin:"5px"}}
+          onClick={() => console.log(transcript)}
+        >
+          Upload
+        </Button>
+        
+      </Box>
+    </Box>
   );
 }
 
