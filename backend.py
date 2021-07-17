ce923dc4b05ceec58ab9e755bfbdfe8cb6c2f123
Yida <5313313@ad.unsw.edu.au>
2021-07-17 18:49:47 +1000

    changed to daniel implementation

--- backend.py
+++ backend.py
@@ -2,67 +2,104 @@ import sqlite3
 
 connection = sqlite3.connect("file.db")
 
+
 def initialiseDb():
-  #table for channel with english related content
-  connection.execute(''' CREATE TABLE english (ID INT PRIMARY KEY NOT NULL, FILENAME STRING NOT NULL)''')
-
+  connection.execute(''' CREATE TABLE content (CHANNELID INT NOT NULL, FILENAME STRING NOT NULL, CATEGORY STRING NOT NULL)''')
   connection.commit()
 
-  #table for channel with maths related content
-  connection.execute(''' CREATE TABLE maths (ID INT PRIMARY KEY  NOT NULL, FILENAME STRING NOT NULL)''')
-
+  connection.execute(''' CREATE TABLE channels (CHANNELID INT PRIMARY KEY NOT NULL, CHANNELNAME STRING NOT NULL)''')
   connection.commit()
 
-  #table for channel to keep track of people that have registered
-  connection.execute('''CREATE TABLE users (NUMBER INT PRIMARY KEY NOT NULL)''')
+  #Hard code the different channels here
+  connection.execute("INSERT INTO channels VALUES (?,?)", (1,"english"))
+  connection.execute("INSERT INTO channels VALUES (?,?)", (2,"maths"))
 
+  #create array to keep track of subscriptions
+  connection.execute(''' CREATE TABLE subscriptions (CHANNELID INT NOT NULL, PHONENUMBER STRING NOT NULL)''')
   connection.commit()
 
-  #table to keep track of people subscribed to english
-  connection.execute('''CREATE TABLE englishsubscriptions (NUMBER INT PRIMARY KEY NOT NULL)''')
-
-  connection.commit()
-
-  #table to keep track of people subscribed to maths channel
-  connection.execute(''' CREATE TABLE mathssubscriptions (NUMBER INT PRIMARY KEY NOT NULL)''')
-
-  connection.commit()
-
+#JSON
+"""
+{
+   "CONTENT":{
+      [
+         "CHANNELID":1,
+         "FILENAME":"Hello.mp3",
+         "CATEGORY":"COMEDY"
+      ]
+   },
+   "CHANNELS":{
+      [
+         "CHANNELID":1,
+         "CHANNELNAME":"english"
+      ],
+      [
+         "CHANNELID":2,
+         "CHANNELNAME":"maths"
+      ]
+   },
+   "SUBSCRIPTIONS":{
+      [
+         "CHANNELID":1,
+         "PHONENUMBER":123456789
+      ],
+      [
+         "CHANNELID":1,
+         "PHONENUMBER":987654632
+      ],
+      [
+         "CHANNELID":2,
+         "PHONENUMBER":515155151
+      ]
+   }
+}
+"""
 #add the data to the appropriate database
-def addDataToEnglish(database, file):
-  # iterate through table and find the next
+def addData(channelId, file, category):
+
+  """
+  # code for finding row number + 1
   cursor = connection.execute(f"SELECT ID from {database}")
   newId = int(len(cursor.fetchall()) + 1)
+  """
 
   #add the new filename in the database
-  connection.execute("INSERT INTO english VALUES (?,?)", (newId, file))
+  #if statements are needed to do the same thing because for some reason 
+  #sqlite does not let you string format INSERT statements 
+  connection.execute("INSERT INTO content VALUES (?,?,?)", (channelId, file, category))
   connection.commit()
 
-#two different methods are needed to do the same thing because for some reason 
-# sqlite does not let you string format INSERT statements 
-def addDataToMaths(database, file):
-  # iterate through table and find the next
-  cursor = connection.execute(f"SELECT ID from {database}")
-  newId = int(len(cursor.fetchall()) + 1)
 
-  #add the new filename in the database
-  connection.execute("INSERT INTO maths VALUES (?,?)", (newId, file))
-  connection.commit()
+def addSubscription(channelId, phoneNumber):
+    connection.execute("INSERT INTO subscriptions VALUES (?,?)",(channelId, phoneNumber))
+    connection.commit()
+
 
 def printDatabase():
-  cursor = connection.execute('''SELECT ID, FILENAME FROM english''')
+  cursor = connection.execute('''SELECT * FROM content''')
+  print("Content Table Details:")
   for row in cursor:
-    print(f"Id: {row[0]}")
-    print(f"File name: {row[1]}")
-  
-  cursor = connection.execute('''SELECT ID, FILENAME FROM maths''')
-  for row in cursor:
-    print(f"Id: {row[0]}")
-    print(f"File name: {row[1]}")
+    print(f"Id: {row[0]} File name: {row[1]} Category: {row[2]}")
 
+  print("")
+
+  print("Channel Table Details")
+  cursor = connection.execute('''SELECT *FROM channels''')
+  for row in cursor:
+    print(f"Channel Id: {row[0]} Channel Name: {row[1]}")
+
+  print("")
+  
+  print("Subscription Table Details")
+  cursor = connection.execute('''SELECT * FROM subscriptions''')
+  for row in cursor:
+    print(f"Channel Id: {row[0]} Phone Number: {row[1]}")
+
+# add dummy data here
 def main():
   initialiseDb()
-  addDataToEnglish("english", "generic filename")
+  addData("english", "generic filename", "comedy")
+  addSubscription(1, 123456789)
   printDatabase()
   connection.close()
 
